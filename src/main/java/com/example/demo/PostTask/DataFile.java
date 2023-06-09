ea7674c79d4bff03c4f26f23ed394686db72e27a
srivarshini7 <srivarshini.mutyala@imaginnovate.com>
2023-06-09 15:25:26 +0530

    Post Json Data Task

---
+++ src/main/java/com/example/demo/PostTask/DataFile.java
@@ -0,0 +1,10 @@
+package com.example.demo.PostTask;
+import jdk.jfr.DataAmount;
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class DataFile {
+    private List< DataFile2> DemoData2;
+
+}
---
+++ src/main/java/com/example/demo/PostTask/DataFile2.java
@@ -0,0 +1,12 @@
+package com.example.demo.PostTask;
+
+
+import lombok.Data;
+
+@Data
+public class DataFile2 {
+    private int userId;
+    private int id;
+    private String title;
+    private String body;
+}
