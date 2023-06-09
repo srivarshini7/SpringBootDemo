adfc7762b6749a9e41838cf18f4211e5c01a7152
srivarshini7 <srivarshini.mutyala@imaginnovate.com>
2023-06-09 12:52:13 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/TestDemo.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestDemo {
+    @GetMapping("/test")
+    public String Test(){
+        return "Hello world";
+    }
+}
