f5e71ca6abbca4e4a01e770a3bfb7ff2de163d3b
srivarshini7 <srivarshini.mutyala@imaginnovate.com>
2023-07-31 23:29:00 +0530

    Code for receive messages

---
+++ src/test/java/ActiveMQReceiver/ReceiverApplicationTest.java
@@ -0,0 +1,12 @@
+package ActiveMQReceiver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class ReceiverApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
