5c94cc3f844a0ccc5e119d3b4d378d1235efee65
srivarshini7 <srivarshini.mutyala@imaginnovate.com>
2023-08-01 11:04:04 +0530

    corrected the desination

--- src/main/java/com/example/demo/Service/MsgReceiverService.java
+++ src/main/java/com/example/demo/Service/MsgReceiverService.java
@@ -4,7 +4,7 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 @Component
 public class MsgReceiverService {
-    @JmsListener(destination = "A new Message")
+    @JmsListener(destination = "The Queue")
     public void listener(String message) {
         System.out.println("The message sent from the Queue - " + message);
     }
